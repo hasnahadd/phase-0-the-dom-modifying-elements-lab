c68fe52c4762e885c6a54850867c244b95b83cba
hasna <manelhasnahaddoud@gmail.com>
2023-08-10 21:58:27 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+main.remove();
+const newHeader =document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent  = `ManelHasna is the champion`;
